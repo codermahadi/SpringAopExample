b461e01a8dc11c8efcf61f74ca9ade3306eb4add
Mahadi Hasan <31430772+codermahadi@users.noreply.github.com>
2017-10-21 17:11:18 +0600

    Annotation Base Poincut
    How you create aop using annotation base configuration

--- springAop/src/main/java/com/mahadi/spring/aop/Camera.java
+++ springAop/src/main/java/com/mahadi/spring/aop/Camera.java
@@ -1,8 +1,12 @@
 package com.mahadi.spring.aop;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Mahadi on 10/21/2017.
  */
+
+@Component
 public class Camera {
     public void snap(){
         System.out.println("SNAP");
--- springAop/src/main/java/com/mahadi/spring/aop/Logger.java
+++ springAop/src/main/java/com/mahadi/spring/aop/Logger.java
@@ -1,10 +1,21 @@
 package com.mahadi.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Mahadi on 10/21/2017.
  */
+@Aspect
+@Component
 public class Logger {
 
+    @Pointcut("execution(void com.mahadi.spring.aop.Camera.snap())")
+    public void cameraSnap(){}
+
+        @Before("cameraSnap()")
         public void aboutToTakePhoto(){
             System.out.println("About To Take photo...");
         }
